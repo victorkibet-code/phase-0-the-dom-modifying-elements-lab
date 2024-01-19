3ef7b1d4adc220c91a81c72c65851cb7b0a67504
Victor <viqtorkibz@gmail.com>
2024-01-19 16:31:33 +0300

    completed assynment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// Write your code here!phase-0-the-dom-modifying-elements-
 // Write your code here!
+// Write your code here!
+const main = document.getElementById('main')
+main.remove();
+
+const body = document.body
+
+const newHeader = document.createElement ('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Macdonald Muhavi is the champion'
+body.appendChild(h1);
